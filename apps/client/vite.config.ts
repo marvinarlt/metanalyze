1b4021c139fd3f8cda894dfee24a4a658cbb827d
Marvin Arlt <marvinarlt@gmx.net>
2022-07-02 14:53:52 +0200

    Destructured path functions

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,21 +1,21 @@
+import { join, resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueI18n from '@intlify/vite-plugin-vue-i18n';
 import svgLoader from 'vite-svg-loader';
-import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueI18n({
-      include: path.join(__dirname, '/src/locales/**')
+      include: join(__dirname, '/src/locales/**')
     }),
     svgLoader({ svgo: false }),
   ],
   resolve: {
     alias: {
-      '@app': path.resolve(__dirname, 'src')
+      '@app': resolve(__dirname, 'src')
     }
   },
   server: {
