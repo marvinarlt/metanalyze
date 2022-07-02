de1b9273c358ebbe2752a7c6c1f3c299351a37e7
Marvin Arlt <marvinarlt@gmx.net>
2022-07-02 15:07:55 +0200

    Refactoring
    Moved tab content of url and snippet tab into seperate components.

---
+++ apps/client/src/components/TheSnippetTab.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import { ArrowRightIcon } from 'vue-tabler-icons';
+
+  const snippet = ref('');
+
+  const startSnippetCheck = () => {
+    console.log(snippet.value);
+  }
+</script>
+
+<template>
+  <form class="form" @submit.prevent="startSnippetCheck">
+    <textarea class="textarea" name="snippet" :placeholder="$t('pages.index.snippet.placeholder')" required v-model="snippet"></textarea>
+    <button class="button align-right" type="submit">{{ $t('pages.index.snippet.submit') }} <ArrowRightIcon /></button>
+  </form>
+</template>
---
+++ apps/client/src/components/TheUrlTab.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import { ArrowRightIcon, CheckIcon } from 'vue-tabler-icons';
+
+  const url = ref('');
+  const shouldCrawl = ref(false);
+
+  const startUrlCheck = () => {
+    // TODO: Validate URL
+    console.log(url.value, shouldCrawl.value);
+  }
+</script>
+
+<template>
+  <form class="form" @submit.prevent="startUrlCheck">
+    <input class="input" type="url" name="url" :placeholder="$t('pages.index.url.placeholder')" required v-model="url">
+    <input class="checkbox-input" type="checkbox" name="crawl" id="crawl" v-model="shouldCrawl">
+    <label class="checkbox" for="crawl">
+      <span class="checkbox-element">
+        <CheckIcon class="checkbox-icon" />
+      </span>
+      <span class="checkbox-label">{{ $t('pages.index.url.checkbox') }}</span>
+    </label>
+    <button class="button align-right" type="submit">{{ $t('pages.index.url.submit') }} <ArrowRightIcon /></button>
+  </form>
+</template>
