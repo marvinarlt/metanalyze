dc188a41a79284a967a626d8be54c7308e5beac0
Marvin Arlt <marvinarlt@gmx.net>
2022-07-03 02:02:11 +0200

    Removed unused function

--- apps/client/src/composables/i18n.ts
+++ apps/client/src/composables/i18n.ts
@@ -1,4 +1,3 @@
-import { watch } from 'vue';
 import { useI18n as useI18nDependency } from 'vue-i18n';
 import { saveLocale, resolveLocale } from '@app/services/i18n';
 
@@ -24,18 +23,6 @@ export function useI18n() {
    */
   const getLocale = (): string => {
     return resolveLocale();
-  }
-
-  /**
-   * Watch for locale changes an save it.
-   * 
-   * @unused
-   * @returns {void}
-   */
-  const watchLocale = (): void => {
-    watch(i18n.locale, () => {
-      saveLocale(i18n.locale.value);
-    });
   }
 
   return {
