f7f6cb9859b3759988762e86059d79d020d7e312
Marvin Arlt <marvinarlt@gmx.net>
2022-07-08 22:34:34 +0200

    Added progress bar component

---
+++ apps/client/src/components/ProgressBar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import { ref, Ref, watch, onBeforeMount } from 'vue';
+
+  const props = defineProps<{
+    value: number
+  }>();
+
+  const progressBar: Ref<HTMLElement | undefined> = ref(undefined);
+
+  const updateProgressBar = () => {
+    if (progressBar.value) {
+      progressBar.value.style.width = `${ props.value * 100 }%`;
+    }
+  }
+
+  onBeforeMount(updateProgressBar);
+  watch(props, updateProgressBar);
+</script>
+
+<template>
+  <div class="progress-bar">
+    <div class="progress-bar-inner" ref="progressBar"></div>
+  </div>
+</template>
