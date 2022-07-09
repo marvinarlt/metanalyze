e7b6f7053427ae97df62bfba496cb12e1a35be3b
Marvin Arlt <marvinarlt@gmx.net>
2022-07-09 19:25:18 +0200

    Styled 404 error page

--- apps/client/src/pages/NotFound.vue
+++ apps/client/src/pages/NotFound.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+  import { ArrowRightIcon } from 'vue-tabler-icons';
+</script>
+
 <template>
-  <h1>Not Found</h1>
+  <div class="page-not-found centered-component">
+    <img class="not-found-image" src="/assets/images/obi-wan-kenobi.gif" alt="Not Found">
+    <h1><span>Page not found</span> "This is not the page you're looking for"</h1>
+    <p>Maybe you have a typo in the url or you just clicked on a link that doesn't exists anymore. Check your url or visit the homepage.</p>
+    <RouterLink class="button" to="/" title="Back to homepage">Back to homepage <ArrowRightIcon /></RouterLink>
+  </div>
 </template>
